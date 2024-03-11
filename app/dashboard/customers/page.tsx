fd7a3ee1fa5010e888a136eaefeeb504802228f2
43alsosc <43alsosc@stud.akademiet.no>
2024-03-11 14:02:30 +0100

    Implementert CustomerTable

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,10 @@
 import CustomerTable from "@/app/ui/customers/customer-table";
+import ShowCustomers from "@/app/ui/customers/show-customers";
 
 export default function page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CustomerTable />
+      <ShowCustomers />
     </main>
   );
 }
--- app/ui/customers/show-customers.tsx
+++ app/ui/customers/show-customers.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import CustomersTable from "./customer-table";
 import { createClient } from "@/utils/supabase/client";
