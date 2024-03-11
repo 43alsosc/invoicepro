5c3a62c0408235bba6acc02285546b1c7c9e21ea
43alsosc <43alsosc@stud.akademiet.no>
2024-03-11 13:53:44 +0100

    customertable

---
+++ app/ui/customers/show-customers.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import CustomersTable from "./customer-table";
+import { createClient } from "@/utils/supabase/client";
+
+// Define the Customer type
+interface Customer {
+  id: number;
+  customerName: string;
+  image_url: string;
+  email: string;
+  total_invoices: number;
+  total_pending: number;
+  total_paid: number;
+}
+
+const supabase = createClient();
+
+const ShowCustomers = () => {
+  const [customers, setCustomers] = useState<Customer[] | null>(null);
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const { data, error } = await supabase.from("customers").select();
+      if (error) {
+        console.error("Error fetching customers:", error.message);
+      } else {
+        setCustomers(data || []);
+      }
+    };
+
+    fetchCustomers();
+  }, []);
+
+  return (
+    <div>
+      {customers !== null ? (
+        <CustomersTable customers={customers} />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default ShowCustomers;
