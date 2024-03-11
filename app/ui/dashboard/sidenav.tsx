cee1fe2ea23de443e304ff080024a5b92989d983
43alsosc <43alsosc@stud.akademiet.no>
2024-03-11 14:38:34 +0100

    gjorde det om til en knapp

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -35,7 +35,9 @@ export default function SideNav() {
         <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 text-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
           <PowerIcon className="w-6" />
           <form action={signOut}>
-            <div className="hidden md:block">Sign Out</div>
+            <button type="submit" className="hidden md:block">
+              Sign Out
+            </button>
           </form>
         </button>
       </div>
