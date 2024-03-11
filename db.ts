34365cbcb9333e47471d23c96c169860a70537cb
43alsosc <43alsosc@stud.akademiet.no>
2024-03-11 08:35:59 +0100

    midlertidig

--- db.ts
+++ db.ts
@@ -1,12 +1,1 @@
-import mysql from "mysql";
-
-const pool = mysql.createPool({
-  host: "172.20.128.80",
-  user: "root",
-  password: "Skole123",
-  database: "invoices",
-});
-
-export const query = (sql: string, values?: any) =>
-  console.log(sql, values)
-  ;
+console.log("Database")
