7613d94e6314e1d9f7315e02dcfedffcae43f1b6
43alsosc <43alsosc@stud.akademiet.no>
2024-03-11 14:35:11 +0100

    La til funksjonen til "Sign Out"

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,8 +2,18 @@ import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import Logo from "@/app/ui/logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default function SideNav() {
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/");
+  };
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       {/* Link for logoen, som sender deg til dashboard hjemmesiden når den blir klikket */}
@@ -24,7 +34,9 @@ export default function SideNav() {
 
         <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 text-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
           <PowerIcon className="w-6" />
-          <div className="hidden md:block">Sign Out</div>
+          <form action={signOut}>
+            <div className="hidden md:block">Sign Out</div>
+          </form>
         </button>
       </div>
     </div>
