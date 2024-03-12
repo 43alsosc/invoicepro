c910835794664996bc5e4793a78aa3b9e8eb63c9
43alsosc <43alsosc@stud.akademiet.no>
2024-03-12 09:05:49 +0100

    Fjernet imports og endret import for AuthButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
+import AuthButton from "./ui/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -23,7 +19,6 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
