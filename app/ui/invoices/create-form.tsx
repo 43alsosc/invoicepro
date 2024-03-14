a1f215ccbbf6d0f6550293c933bd21b50218b35e
43alsosc <43alsosc@stud.akademiet.no>
2024-03-14 23:09:44 +0100

    Oppdaterte status knappene til å se bedre ut

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -5,6 +5,7 @@ import {
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
+  ExclamationTriangleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
 import { createClient } from "@/utils/supabase/client";
@@ -148,62 +149,88 @@ export default function Form() {
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Amount
           </label>
-          <input
-            id="amount"
-            name="amount"
-            type="number"
-            step="0.01"
-            placeholder="Enter USD amount"
-            className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-            onChange={handleAmountChange}
-          />
-          <div>
-            {amountError && <div style={{ color: "red" }}>{amountError}</div>}
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="amount"
+                name="amount"
+                type="number"
+                step="0.01"
+                placeholder="Enter USD amount"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                onChange={handleAmountChange}
+              />
+              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div>
+              {amountError && <div style={{ color: "red" }}>{amountError}</div>}
+            </div>
           </div>
         </div>
 
         {/* Status */}
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-medium">Status</label>
-          <div>
-            <label htmlFor="pending" className="inline-flex items-center">
-              <input
-                id="pending"
-                name="status"
-                type="radio"
-                value="pending"
-                className="peer block text-sm outline-2"
-                onChange={handleStatusChange}
-              />
-              <span className="ml-2">Pending</span>
-            </label>
-            <label htmlFor="paid" className="inline-flex items-center ml-6">
-              <input
-                id="paid"
-                name="status"
-                type="radio"
-                value="paid"
-                className="peer block text-sm outline-2"
-                onChange={handleStatusChange}
-              />
-              <span className="ml-2">Paid</span>
-            </label>
-            <label htmlFor="overdue" className="inline-flex items-center ml-6">
-              <input
-                id="overdue"
-                name="status"
-                type="radio"
-                value="overdue"
-                className="peer block text-sm outline-2"
-                onChange={handleStatusChange}
-              />
-              <span className="ml-2">Overdue</span>
-            </label>
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium">
+            Set the invoice status
+          </legend>
+          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+            <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="pending"
+                  name="status"
+                  type="radio"
+                  value="pending"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  onChange={handleStatusChange}
+                />
+                <label
+                  htmlFor="pending"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600 dark:text-gray-300"
+                >
+                  Pending <ClockIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="paid"
+                  name="status"
+                  type="radio"
+                  value="paid"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  onChange={handleStatusChange}
+                />
+                <label
+                  htmlFor="paid"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white dark:text-gray-300"
+                >
+                  Paid <CheckIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="overdue"
+                  name="status"
+                  type="radio"
+                  value="overdue"
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  onChange={handleStatusChange}
+                />
+                <label
+                  htmlFor="overdue"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-red-500 px-3 py-1.5 text-xs font-medium text-white dark:text-gray-300"
+                >
+                  Overdue <ExclamationTriangleIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div>
+                {statusError && (
+                  <div style={{ color: "red" }}>{statusError}</div>
+                )}
+              </div>
+            </div>
           </div>
-          <div>
-            {statusError && <div style={{ color: "red" }}>{statusError}</div>}
-          </div>
-        </div>
+        </fieldset>
 
         {/* Due Date */}
         <div className="mb-4">
