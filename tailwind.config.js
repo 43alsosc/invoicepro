1176c8d7d2ec557ae25ae98598dfc691488421f9
43alsosc <43alsosc@stud.akademiet.no>
2024-03-15 13:21:13 +0100

    Intergrert TW Elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,13 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/tw-elements/js/**/*.js",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
-        },
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("tw-elements/plugin.cjs")],
 };
