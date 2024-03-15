cd1c37451a47318ddab0697dd206658da63c30d1
43alsosc <43alsosc@stud.akademiet.no>
2024-03-15 10:46:21 +0100

    Fikset signout knappen som bare fungerte på teksten og ikke hele knappen

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -32,12 +32,12 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-100  md:block"></div>
 
-        <div className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 text-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-          <PowerIcon className="w-6" />
-          <form action={signOut}>
-            <button>Sign Out</button>
-          </form>
-        </div>
+        <form action={signOut}>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            Sign Out
+          </button>
+        </form>
       </div>
     </div>
   );
