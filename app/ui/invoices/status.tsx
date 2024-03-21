2f7928d5d72aec451204d63ecf87146daf91ca40
43alsosc <43alsosc@stud.akademiet.no>
2024-03-21 12:55:27 +0100

    byttet ikon

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,34 +1,38 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import {
+  CheckIcon,
+  ClockIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-          'bg-red-500 text-white': status === 'overdue',
-        },
+          "bg-gray-100 text-gray-500": status === "pending",
+          "bg-green-500 text-white": status === "paid",
+          "bg-red-500 text-white": status === "overdue",
+        }
       )}
     >
-      {status === 'pending' ? (
+      {status === "pending" ? (
         <>
           Pending
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
-      {status === 'paid' ? (
+      {status === "paid" ? (
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
-      {status === 'overdue' ? (
+      {status === "overdue" ? (
         <>
           Overdue
-          <CheckIcon className="ml-1 w-4 text-white" />
+          <ExclamationTriangleIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
